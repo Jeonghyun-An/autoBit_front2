61bf7c56ff29409bd02f7ca496c91cda50aab273
Hyonii <131145818+Jeonghyun-An@users.noreply.github.com>
2025-08-04 15:16:14 +0900

    [feat] 프로젝트 세팅

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="p-6 text-center">
+    <h1 class="text-4xl font-bold text-blue-600">🚀 Tailwind + Nuxt 세팅 완료!</h1>
+    <p class="mt-2 text-gray-500">이제부터 마음껏 시작하세요.</p>
+    
+  </div>
+</template>
+<script setup lang="ts">
+const config = useRuntimeConfig()
+console.log("✅ 백엔드 주소:", config.public.backendHost)
+</script>
