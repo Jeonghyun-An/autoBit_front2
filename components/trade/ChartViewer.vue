a346a96d79a8f18a4e5f9e885b3a43c02f819505
Hyonii <131145818+Jeonghyun-An@users.noreply.github.com>
2025-08-28 14:35:59 +0900

    [delete] 재활용

--- components/trade/ChartViewer.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <img :src="`data:image/png;base64,${src}`" alt="Trade Chart" />
-  </div>
-</template>
-
-<script setup lang="ts">
-defineProps<{ src: string }>()
-</script>
